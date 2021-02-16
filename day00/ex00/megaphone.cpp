a26c1a966f9d372a34cb306a2ccabe65334a2931
Rinat <nuretdin1405@gmail.com>
2021-02-16 12:57:31 +0300

    first commit

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <istream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	std::string str;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 1;
+	}
+	for (int i = 1; i < argc; i++)
+		str = str.append(argv[i]);
+	for (std::string::size_type i = 0; i < str.size(); i++)
+		std::cout << static_cast<char>(toupper(static_cast<char>(str[i])));
+	std::cout << std::endl;
+	return 0;
+}

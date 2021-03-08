931ad3cde6c1cbc7217bfee3d5870bf99f47a27d
Rinat <nuretdin1405@gmail.com>
2021-03-08 13:36:33 +0300

    ex07 Makefile changed, added file test

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+//		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 1;
 	}
 	for (int i = 1; i < argc; i++)

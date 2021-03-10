612647a5b4a0eaaca16b9bbfa43f7299f2b7ae15
Rinat <nuretdin1405@gmail.com>
2021-03-10 19:05:55 +0300

    day02 ex00 finished

---
+++ day02/ex01/Fixed.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Rinat Nurutdinow on 3/8/21.
+//
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _nnumOfFractionalBeats(8) {
+	std::cout << "Default constructor called" << std::endl;
+	this->_nfixedPointValue = 0;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& copy_class)
+	: _nnumOfFractionalBeats(copy_class._nnumOfFractionalBeats){
+	std::cout << "Copy constructor called" << std::endl;
+	this->_nfixedPointValue = copy_class.getRawBits();
+}
+
+Fixed& Fixed::operator=(Fixed const & obj_to_assign) {
+	std::cout << "Assignation operator called" << std::endl;
+	this->_nfixedPointValue = obj_to_assign.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits() const{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _nfixedPointValue;
+}
+
+void Fixed::setRawBits(const int raw) {
+	_nfixedPointValue = raw;
+}
---
+++ day02/ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Rinat Nurutdinow on 3/8/21.
+//
+
+#ifndef _FIXED_H
+#define _FIXED_H
+
+#include <iostream>
+
+class Fixed {
+public:
+
+	Fixed();
+	Fixed(const Fixed& copy_class);
+	~Fixed();
+
+	Fixed& 	operator=(Fixed const & obj_to_assign);
+	void setRawBits( int const raw );
+	int getRawBits() const;
+
+private:
+
+	int 	_nfixedPointValue;
+	const int _nnumOfFractionalBeats;
+
+};
+
+
+#endif //_FIXED_H
